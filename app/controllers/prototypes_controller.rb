b7648c95ffc0ca739fee083fd26ce961380d6380
Tomogit2 <tomotomotomotomo_@hotmail.co.jp>
2024-06-13 17:18:05 +0900

    Prototypeモデルに、has_one_attachedを使用してimageカラムとのアソシエーションを記述した

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,4 +1,11 @@
 class PrototypesController < ApplicationController
   def index
   end
+
+  private
+
+  def prototype_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user: current_user)
+  end
+  
 end
