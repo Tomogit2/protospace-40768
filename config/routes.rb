613ffec1a435e67875296e8512fb56fe318a5edd
Tomogit2 <tomotomotomotomo_@hotmail.co.jp>
2024-06-14 08:29:54 +0900

    resourcesを用いて、上記で設定したnewアクションとcreateアクションに対するルーティングをroutes.rbに記述した

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:show]  # ユーザー詳細ページのルート設定
+  resources :prototypes, only: [:index, :new, :create]
   get 'prototypes/index'
   root to: "prototypes#index"
 end
