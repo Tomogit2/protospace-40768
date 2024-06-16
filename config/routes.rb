14f9a3d22a718dde044c6027926d658c9e5a99f4
Tomogit2 <tomotomotomotomo_@hotmail.co.jp>
2024-06-16 19:30:57 +0900

    resourcesを用いて、上記で設定したcreateアクションに対するルーティングをroutes.rbに記述した

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:show]  # ユーザー詳細ページのルート設定
-  resources :prototypes, only: [:index, :new, :create, :show, :edit, :update, :destroy]
+  resources :prototypes, only: [:index, :new, :create, :show, :edit, :update, :destroy] do
+    resources :comments, only: [:create]
   get 'prototypes/index'  #この行は不要かもしれない。すでにresources :prototypesでindexアクションへのルートは生成されているからです。この行を削除しても、indexアクションへのルーティングには影響はありません。
   root to: "prototypes#index"
 end
