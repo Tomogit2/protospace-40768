bc517293058a2f5449c5943e89ef2abd58b50b49
Tomogit2 <tomotomotomotomo_@hotmail.co.jp>
2024-06-16 11:53:00 +0900

    show.html.erbの「編集する」ボタンから、編集ページに遷移するようにパスを設定した（パスはrails routesを用いて確認する）

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:show]  # ユーザー詳細ページのルート設定
-  resources :prototypes, only: [:index, :new, :create, :show]
+  resources :prototypes, only: [:index, :new, :create, :show, :edit, :update]
   get 'prototypes/index'  #この行は不要かもしれない。すでにresources :prototypesでindexアクションへのルートは生成されているからです。この行を削除しても、indexアクションへのルーティングには影響はありません。
   root to: "prototypes#index"
 end
