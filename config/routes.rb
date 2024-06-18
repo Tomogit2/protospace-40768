b28657c56c1705a79a4e23ab6af214eb4a72f6f7
Tomogit2 <tomotomotomotomo_@hotmail.co.jp>
2024-06-18 09:11:48 +0900

    【first commit】実装課題を提出6/18

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   resources :users, only: [:show]  # ユーザー詳細ページのルート設定
   resources :prototypes, only: [:index, :new, :create, :show, :edit, :update, :destroy] do
     resources :comments, only: [:create]
-  get 'prototypes/index'  #この行は不要かもしれない。すでにresources :prototypesでindexアクションへのルートは生成されているからです。この行を削除しても、indexアクションへのルーティングには影響はありません。
   end
   root to: "prototypes#index"
 end
